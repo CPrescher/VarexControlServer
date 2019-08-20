f6e13fa5477f6f15bbe6426ec94e3b7f4b868ce1
Clemens Prescher <clemens.prescher@gmail.com>
2019-08-20 17:58:58 +0200

    Running Server now on Varex Control Computer

--- simple_collection.py
+++ simple_collection.py
@@ -1,6 +1,7 @@
 import requests
 
-server_address = 'http://127.0.0.1:5000'
+#server_address = 'http://127.0.0.1:5000'
+server_address = 'http://131.169.215.66:5000'
 
 
 def set_attribute(attribute, value):
@@ -12,8 +13,8 @@ def collect_data():
     requests.get('{}/StartAcq/1'.format(server_address))
 
 
-set_attribute('nFrames1', 10)
-set_attribute('ExposureTime1', 1)
+set_attribute('nFrames1', 30)
+set_attribute('ExposureTime1', 3)
 set_attribute('FileDir1', 'D:\\temp_images')
 set_attribute('FileIndex1', 1)
 collect_data()
